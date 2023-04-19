4fec130c2ccdf90eaeafd1952ea5d3e228478405
AgeHT <vladimir.e.pavlov@yandex.ru>
2023-04-20 02:38:33 +0700

    VK auth

--- todolist/urls.py
+++ todolist/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/', include('core.urls', namespace='core')),
+    path('oauth/', include('social_django.urls', namespace='social')),
 ]
 
 if settings.DEBUG:
