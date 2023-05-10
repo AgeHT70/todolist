2e4217e01b502c9bbd3e6a5905739bcc61dcb37e
AgeHT <vladimir.e.pavlov@yandex.ru>
2023-05-10 23:08:01 +0700

    bot /goals,/create,/cancel

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -3,13 +3,14 @@ from django.core.management.base import BaseCommand
 from bot.models import TgUser
 from bot.tg.client import TgClient
 from bot.tg.schemas import Message
-from goals.models import Goal
+from goals.models import Goal, GoalCategory
 
 
 class Command(BaseCommand):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.tg_client = TgClient()
+        self._wait_list = {}
 
     def handle(self, *args, **options):
         offset = 0
@@ -28,13 +29,64 @@ class Command(BaseCommand):
             self.handle_unauthorized_user(tg_user, msg)
 
     def handle_authorized_user(self, tg_user: TgUser, msg: Message):
-        if msg.text == '/goals':
-            qs = Goal.objects.filter(
-                category__is_deleted=False, category__board__participants__user_id=tg_user.user.id
-            ).exclude(status=Goal.Status.archived)
-            goals = [f'{goal.id} - {goal.title}' for goal in qs]
-            self.tg_client.send_message(chat_id=msg.chat.id, text='No goals' if not goals else '\n'.join(goals))
-        else:
+        commands: list = ['/goals', '/create', '/cancel']
+        create_chat: dict | None = self._wait_list.get(msg.chat.id, None)
+
+        if msg.text == '/cancel':
+            self._wait_list.pop(msg.chat.id, None)
+            create_chat = None
+            self.tg_client.send_message(chat_id=msg.chat.id, text='Operation was canceled')
+
+        if msg.text in commands and not create_chat:
+            if msg.text == '/goals':
+                qs = Goal.objects.filter(
+                    category__is_deleted=False, category__board__participants__user_id=tg_user.user.id
+                ).exclude(status=Goal.Status.archived)
+                goals = [f'{goal.id} - {goal.title}' for goal in qs]
+                self.tg_client.send_message(chat_id=msg.chat.id, text='No goals' if not goals else '\n'.join(goals))
+
+            if msg.text == '/create':
+                categories_qs = GoalCategory.objects.filter(
+                    board__participants__user_id=tg_user.user.id, is_deleted=False
+                )
+
+                categories = []
+                categories_id = []
+                for category in categories_qs:
+                    categories.append(f'{category.id} - {category.title}')
+                    categories_id.append(str(category.id))
+
+                self.tg_client.send_message(
+                    chat_id=msg.chat.id, text=f'Choose number of category:\n' + '\n'.join(categories)
+                )
+                self._wait_list[msg.chat.id] = {
+                    'categories': categories,
+                    'categories_id': categories_id,
+                    'category_id': '',
+                    'goal_title': '',
+                    'stage': 1,
+                }
+        if msg.text not in commands and create_chat:
+            if create_chat['stage'] == 2:
+                Goal.objects.create(
+                    user_id=tg_user.user.id,
+                    category_id=int(self._wait_list[msg.chat.id]['category_id']),
+                    title=msg.text,
+                )
+                self.tg_client.send_message(chat_id=msg.chat.id, text='Goal save')
+                self._wait_list.pop(msg.chat.id, None)
+
+            elif create_chat['stage'] == 1:
+                if msg.text in create_chat.get('categories_id', []):
+                    self.tg_client.send_message(chat_id=msg.chat.id, text='Enter title for goal')
+                    self._wait_list[msg.chat.id] = {'category_id': msg.text, 'stage': 2}
+                else:
+                    self.tg_client.send_message(
+                        chat_id=msg.chat.id,
+                        text='Enter correct number of category\n' + '\n'.join(create_chat.get('categories', [])),
+                    )
+
+        if msg.text not in commands and not create_chat:
             self.tg_client.send_message(chat_id=msg.chat.id, text=f'Unknown command!')
 
     def handle_unauthorized_user(self, tg_user: TgUser, msg: Message):
